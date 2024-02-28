c50b23fe4ed3442d39eab9713272c30826e7e8f7
Rodri Casas <cr.rodrigocasas@gmail.com>
2024-02-28 19:01:37 -0300

    varios cambios

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,14 @@
+<script setup>
+import { useCounterStore } from '@/stores/counter.js';
+
+const counterStore = useCounterStore();
+
+</script>
 <template>
-  <div class="about">
+  <div class="flex flex-col text-2xl">
     <h1>This is an about page</h1>
+
+    <button class="text-2xl" @click="counterStore.increment()">Contador</button>
+    <p>El contador es: {{ counterStore.count }} y el doble es {{ counterStore.doubleCount }}</p>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
