bb1101c57998bed315da5583a2bee6ebc3118f5c
Rodri Casas <cr.rodrigocasas@gmail.com>
2024-05-09 23:45:33 -0300

    api axios wrapper

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,49 @@
 import axios from 'axios'
 
+// const api = axios.create({
+//   baseURL: 'http://localhost:8000/api/v1',
+//   withCredentials: true,
+//   headers: {
+//     'Content-Type': 'application/json'
+//   }
+// })
 
-const api = axios.create({
-  baseURL: 'http://localhost:8000/api/v1',
-  withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json'
+class Api {
+  constructor() {
+    this.api = axios.create({
+      baseURL: 'http://localhost:8000/api/v1',
+      withCredentials: true,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
   }
-})
+
+  get(url, config) {
+    console.log(`%cGET %c${url}`, 'color: cyan;', 'color: lightgreen;')
+    return this.api.get(url, config)
+  }
+
+  post(url, data, config) {
+    console.log(`%cPOST %c${url}`, 'color: teal;', 'color: lightgreen;')
+    return this.api.post(url, data, config)
+  }
+
+  put(url, data, config) {
+    console.log(`%cPUT %c${url}`, 'color: gold;', 'color: lightgreen;')
+    return this.api.put(url, data, config)
+  }
+
+  delete(url, config) {
+    console.log(`%cDELETE %c${url}`, 'color: red;', 'color: lightgreen;')
+    return this.api.delete(url, config)
+  }
+}
+
+
 
 // api.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 // api.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 // api.defaults.headers.common['Access-Control-Allow-Credentials'] = 'true';
 
-export default api
+export default new Api()
