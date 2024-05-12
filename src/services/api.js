d5e894dbc8e76443c3803c521108e4a7848239d5
Rodri Casas <cr.rodrigocasas@gmail.com>
2024-05-12 03:24:41 -0300

    api auto host y logger

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,26 @@
 import axios from 'axios'
 
-// const api = axios.create({
-//   baseURL: 'http://localhost:8000/api/v1',
-//   withCredentials: true,
-//   headers: {
-//     'Content-Type': 'application/json'
-//   }
-// })
+export const PORT = import.meta.env['API_PORT'] || 8000
+export const HOST = `http://${window.location.hostname}:${PORT}`
+export const URL = `${HOST}/api/v1`
+
+const OPTS = {
+  log: true,
+  respuesta: true,
+  respuestaData: true,
+  colors: {
+    get: 'cyan',
+    post: 'teal',
+    put: 'gold',
+    delete: 'red',
+    url: 'lightgreen'
+  }
+}
 
 class Api {
   constructor() {
     this.api = axios.create({
-      baseURL: 'http://localhost:8000/api/v1',
+      baseURL: URL,
       withCredentials: true,
       headers: {
         'Content-Type': 'application/json'
@@ -19,10 +28,46 @@ class Api {
     })
   }
 
-  get(url, config) {
-    console.log(`%cGET %c${url}`, 'color: cyan;', 'color: lightgreen;')
-    return this.api.get(url, config)
+  async mostrarRespuesta(method, url, promise) {
+    let log = console.log
+    let obj = null
+
+    try {
+      obj = await promise
+    } catch (error) {
+      log = console.error
+      obj = error
+    }
+
+
+    if (OPTS.log) {
+      const out = []
+      out.push(`%c${method.toUpperCase()} %c${url}`)
+      out.push(`color: ${OPTS.colors[method]};`)
+      out.push(`color: ${OPTS.colors['url']};`)
+      if (OPTS.respuesta) {
+        if (OPTS.respuestaData) {
+          if (obj.data) {
+            out.push(obj.data)
+          } else {
+            out.push(obj)
+          }
+        } else {
+          out.push(obj)
+        }
+      }
+      log(...out)
+    }
   }
+
+  get(url, config) {
+    // console.log(`%cGET %c${url}`, 'color: cyan;', 'color: lightgreen;')
+    const promise = this.api.get(url, config)
+
+    this.mostrarRespuesta('get', url, promise)
+    return promise
+  }
+
 
   post(url, data, config) {
     console.log(`%cPOST %c${url}`, 'color: teal;', 'color: lightgreen;')
